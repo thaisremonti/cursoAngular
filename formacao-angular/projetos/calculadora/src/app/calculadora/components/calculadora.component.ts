f8825c1224aadb9794ddf92daef6a949f75743ec
Thais Remonti <thais.remonti@mv.com.br>
2019-07-26 18:59:44 -0300

    config calculadora

--- formacao-angular/projetos/calculadora/src/app/calculadora/components/calculadora.component.ts
+++ formacao-angular/projetos/calculadora/src/app/calculadora/components/calculadora.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CalculadoraService } from '../services';
+import { runInThisContext } from 'vm';
 
 @Component({
   selector: 'app-calculadora',
@@ -7,11 +8,89 @@ import { CalculadoraService } from '../services';
   styleUrls: ['./calculadora.component.css']
 })
 export class CalculadoraComponent implements OnInit {
+
+  private numero1: string;
+  private numero2: string;
+  private resultado: number;
+  private operacao: string;
+
   //o typescript vai criar um atributo para a classe chamado calculadora service
   //que pode ser usado em qualquer outro metodo usando o operador this
   constructor(private calculadoraService: CalculadoraService) { }
 
   ngOnInit() {
+    this.limpar();
   }
 
+  limpar(): void {
+    this.numero1 = '0';
+    this.numero2 = null;
+    this.resultado = null;
+    this.operacao = null;
+  }
+
+  adicionaNumero(numero: string): void {
+    if(this.operacao === null){
+      this.numero1 = this.concatenaNumero(this.numero1, numero)
+    }else{
+      this.numero2 = this.concatenaNumero(this.numero2, numero)
+    }
+  }
+
+  concatenaNumero(numAtual: string, numConcat: string): string{
+    if(numAtual === '0' || numAtual === null){
+      numAtual = ';'
+    }
+
+    if(numConcat === '.' && numAtual === ''){
+      return '0.'
+    }
+
+    if(numConcat === '.' && numAtual.indexOf('.') > -1){
+      numAtual;
+    }
+
+    return numAtual + numConcat;
+  }
+
+  definirOperacao(operacao: string): void {
+    if(this.operacao === null) {
+      this.operacao = operacao;
+      return;
+    }
+
+    if(this.numero2 != null){
+      this.resultado = this.calculadoraService.calcular(
+        parseFloat(this.numero1),
+        parseFloat(this.numero2),
+        this.operacao);
+      this.operacao = operacao;
+      this.numero1 = this.resultado.toString();
+      this.numero2 = null;
+      this.resultado = null;
+    }
+  }
+
+  calcular(): void {
+    if(this.numero2 === null) {
+      return;
+    }
+
+    this.resultado = this.calculadoraService.calcular(
+      parseFloat(this.numero1),
+      parseFloat(this.numero2),
+      this.operacao);
+  }
+
+  get display(): string {
+    if(this.resultado !== null) {
+      return this.resultado.toString();
+    }
+
+    if(this.numero2 !== null) {
+      return this.numero2;
+    }
+
+    return this.numero1;
+  }
 }
--- formacao-angular/projetos/calculadora/src/app/calculadora/services/calculadora.service.spec.ts
+++ formacao-angular/projetos/calculadora/src/app/calculadora/services/calculadora.service.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
 
 import { CalculadoraService } from './calculadora.service';
 
@@ -11,5 +11,39 @@ describe('CalculadoraService', () => {
   });
 });
 
+it('deve garantir que 1+4 = 5',
+  inject ([CalculadoraService], (service: CalculadoraService) => {
+    let soma = service.calcular(1, 4, CalculadoraService.SOMA);
+    expect(soma).toEqual(5);
+  })
+);
+
+it('deve garantir que 1 - 4 = -3',
+  inject ([CalculadoraService], (service: CalculadoraService) => {
+    let soma = service.calcular(1, 4, CalculadoraService.SUBTRACAO);
+    expect(soma).toEqual(-3);
+  })
+);
+
+it('deve garantir que 1 * 4 = 4',
+  inject ([CalculadoraService], (service: CalculadoraService) => {
+    let soma = service.calcular(1, 4, CalculadoraService.MULTIPLICACAO);
+    expect(soma).toEqual(4);
+  })
+);
+
+it('deve garantir que 10 / 2 = 5',
+  inject ([CalculadoraService], (service: CalculadoraService) => {
+    let soma = service.calcular(10, 2, CalculadoraService.DIVISAO);
+    expect(soma).toEqual(5);
+  })
+);
+
+it('deve retornar 0 para operação inválida',
+  inject ([CalculadoraService], (service: CalculadoraService) => {
+    let soma = service.calcular(10, 2, '%');
+    expect(soma).toEqual(0);
+  })
+);
 
 // spc -> arquivo de teste
