dfd83b910a0aae102d59cd9b65a57d2f8a2ea064
Thais Remonti <thais.remonti@mv.com.br>
2019-07-26 18:09:06 -0300

    configuraçoes

---
+++ formacao-angular/projetos/calculadora/src/app/calculadora/services/calculadora.service.spec.ts
@@ -0,0 +1,15 @@
+import { TestBed } from '@angular/core/testing';
+
+import { CalculadoraService } from './calculadora.service';
+
+describe('CalculadoraService', () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
+
+  it('should be created', () => {
+    const service: CalculadoraService = TestBed.get(CalculadoraService);
+    expect(service).toBeTruthy();
+  });
+});
+
+
+// spc -> arquivo de teste
